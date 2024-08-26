e04a7ab0da71fd9c32f304f73f90a2b77d97f5f2
Klejtek <michal.klejnocki@gmail.com>
2024-08-26 09:03:57 +0200

    Delete hashpassword.js

--- hashpassword.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require('bcrypt');
-
-const plainPassword = 'your_password'; // Zamień 'your_password' na rzeczywiste hasło
-bcrypt.hash(plainPassword, 10, (err, hash) => {
-    if (err) throw err;
-    console.log('Zahashowane hasło:', hash);
-});
